ab10d5cb0c82c5b261097e76fa7cf0fd1c85fdfd
codenameransom <codejoshuaroque@gmail.com>
2021-07-12 14:49:27 -0400

    this was stumping me for a good 4 hours!

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    const match = record.find((obj) => obj.result === "W")
+    if (match == undefined) {
+        return undefined
+    }
+    else {
+        return match.year
+    }
+}
